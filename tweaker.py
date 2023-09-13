5a240c47049c242064db8d0173877cf764c7be1b
dexterdreeeam <dexterdreeeam@foxmail.com>
2023-09-14 00:02:50 +0800

    u

--- tweaker.py
+++ tweaker.py
@@ -1,14 +1,14 @@
 #region Init
-
 import importlib
 import pip
 import ast
 import os
+import time
 
 pip.main(["install", "--upgrade", "pip"])
 
 current_file_path = os.path.abspath(__file__)
-with open(current_file_path, "r") as f:
+with open(current_file_path, "r", encoding="utf-8") as f:
     current_file_str = f.read()
 
 for node in ast.walk(ast.parse(current_file_str)):
@@ -25,11 +25,9 @@ for node in ast.walk(ast.parse(current_file_str)):
             importlib.import_module(p)
         except ImportError:
             pip.main(["install", p])
-
 #endregion
 
 #region Config
-
 import json
 
 root_path = os.path.dirname(os.path.abspath(__file__))
@@ -55,16 +53,31 @@ else:
 print(f"Using environment: {config['name']}")
 adb_path = config["adb_path"] if "adb_path" in config else buildin_adb_path
 keys = config["keys"] if "keys" in config else {}
+#endregion
 
+#region Mapper
+class Mapper:
+    def __init__(self, keys):
+        self.keys = keys
+    def map(self, k):
+        if k in self.keys:
+            return self.keys[k]
+        else:
+            return None
+    def add(self, k, v):
+        self.keys[k] = v
+    def remove(self, k):
+        if k in self.keys:
+            del self.keys[k]
 #endregion
 
 #region ADB
-
 class Adb:
-    def __init__(self):
+    def __init__(self, mapper):
         self.adb_prefix = f"\"{adb_path}\""
         self.screen_width = 0
         self.screen_height = 0
+        self.mapper = mapper
         self.__adb_setup()
 
     def __adb(self, cmd):
@@ -82,7 +95,7 @@ class Adb:
     def __adb_screen_size(self):
         size_str = self.__adb_r("shell wm size")
         size_arr = size_str.split()[2].split("x")
-        self.screen_witdh = int(size_arr[1])
+        self.screen_width = int(size_arr[1])
         self.screen_height = int(size_arr[0])
 
     def __adb_setup(self):
@@ -103,15 +116,50 @@ class Adb:
         self.adb_prefix = f"{self.adb_prefix} -s {target_device}"
         self.__adb_screen_size()
 
+    def adb_action(self, key):
+        release = False
+        if key[0] == "!":
+            release = True
+            key = key[1:]
+        act = self.mapper.map(key)
+        if act is None:
+            return
+        act = act.split()
+        act_type = act[0]
+        if act_type == "click":
+            self.adb_click(release, act[1], act[2])
+        elif act_type == "swipe":
+            self.adb_swipe(release, act[1], act[2], act[3], act[4])
+        elif act_type == "swipe_direction":
+            self.adb_swipe_direction(release, act[1], act[2])
+        elif act_type == "swipe_area":
+            self.adb_swipe_area(release, act[1], act[2])
+        else:
+            print(f"Unknown action: {act_type}")
+
+    def adb_click(self, release, x, y):
+        if release:
+            return
+        x = float(x) * self.screen_width
+        y = float(y) * self.screen_height
+        self.__adb(f"shell input tap {x} {y}")
+
+    def adb_swipe(self, release, x, y, x_d, y_d):
+        if release:
+            return
+        x = float(x) * self.screen_width
+        y = float(y) * self.screen_height
+        x_d = float(x_d) * self.screen_width
+        y_d = float(y_d) * self.screen_height
+        self.__adb(f"shell input swipe {x} {y} {x_d} {y_d} 20")
 #endregion
 
 #region Windows
-
 import threading
 from pynput import keyboard as Keyboard, mouse as Mouse
 
 class Desktop:
-    def __init__(self):
+    def __init__(self, adb):
         self.window_left = 0
         self.window_top = 0
         self.window_right = 0
@@ -131,10 +179,14 @@ class Desktop:
 
         self.mouse_x = 0
         self.mouse_y = 0
+        self.mouse_x_pct = 0.0
+        self.mouse_y_pct = 0.0
         self.mouse_left = False
         self.mouse_right = False
         self.mouse_worker = threading.Thread(target=self.mouse_work, args=(self,))
         self.mouse_worker.start()
+
+        self.adb = adb
 
     def key_work(arg, _):
         key_listener = Keyboard.Listener(
@@ -155,12 +207,32 @@ class Desktop:
         self.window_width = self.window_right - self.window_left
         self.window_height = self.window_bottom - self.window_top
 
-    def __record_key(self, key, is_pressed):
+    def format_key(key):
         if Desktop.is_ctrl(key):
-            self.key_ctrl = is_pressed
+            return "ctrl"
         elif Desktop.is_shift(key):
-            self.key_shift = is_pressed
+            return "shift"
         elif Desktop.is_alt(key):
+            return "alt"
+        elif Desktop.is_digit(key) >= 0:
+            return str(Desktop.is_digit(key))
+        elif Desktop.is_fn(key) >= 0:
+            return "f" + str(Desktop.is_fn(key))
+        else:
+            a = Desktop.is_alpha(key)
+            if a >= 0:
+                return chr(a + ord('a'))
+        return ""
+
+    def __record_key(self, key, is_pressed):
+        fk = Desktop.format_key(key)
+        if fk == "":
+            return ""
+        elif fk == "ctrl":
+            self.key_ctrl = is_pressed
+        elif fk == "shift":
+            self.key_shift = is_pressed
+        elif fk == "alt":
             self.key_alt = is_pressed
         elif Desktop.is_digit(key) >= 0:
             self.key_digits[Desktop.is_digit(key)] = is_pressed
@@ -180,21 +252,34 @@ class Desktop:
                     self.window_bottom = self.mouse_y
                     self.__calc_window_size()
                 self.key_letters[a] = is_pressed
+        return "!" + fk if not is_pressed else fk
 
     def on_press(self, key):
-        self.__record_key(key, True)
-        print(f"Key press is: {key}")
+        fk = self.__record_key(key, True)
+        self.adb.adb_action(fk)
+        # print(f"Key press is: {key}")
         return True
 
     def on_release(self, key):
-        self.__record_key(key, False)
-        print(f"Key release is: {key}")
+        fk = self.__record_key(key, False)
+        self.adb.adb_action(fk)
+        # print(f"Key release is: {key}")
         return True
 
     def on_move(self, x, y):
         self.mouse_x = x
         self.mouse_y = y
-        print(f"Mouse position is: {x}, {y}")
+
+        if self.window_width > 0:
+            self.mouse_x_pct = (x - self.window_left) / self.window_width
+        else:
+            self.mouse_x_pct = 0.0
+
+        if self.window_height > 0:
+            self.mouse_y_pct = (y - self.window_top) / self.window_height
+        else:
+            self.mouse_y_pct = 0.0
+        # print(f"Mouse position is: {x}, {y}")
         return True
 
     def on_click(self, x, y, button, pressed):
@@ -202,11 +287,11 @@ class Desktop:
             self.mouse_left = pressed
         elif button == Mouse.Button.right:
             self.mouse_right = pressed
-        print(f"Mouse click is: {x}, {y}, {button}, {pressed}")
+        # print(f"Mouse click is: {x}, {y}, {button}, {pressed}")
         return True
 
     def on_scroll(self, x, y, dx, dy):
-        print(f"Mouse scroll is: {x}, {y}, {dx}, {dy}")
+        # print(f"Mouse scroll is: {x}, {y}, {dx}, {dy}")
         return True
 
     def is_ctrl(key):
@@ -259,12 +344,13 @@ class Desktop:
             return 12
         else:
             return -1
-
 #endregion
 
 print("Start Running...")
+mapper = Mapper(keys)
+adb = Adb(mapper)
+desk = Desktop(adb)
 
-adb = Adb()
-desk = Desktop()
 while True:
-    pass
+    time.sleep(1)
+
