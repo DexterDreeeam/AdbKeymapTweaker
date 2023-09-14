5fa090e3fe544ab739a0357b80ecf3fd50c65909
dexterdreeeam <dexterdreeeam@foxmail.com>
2023-09-14 23:53:03 +0800

    u

--- tweaker.py
+++ tweaker.py
@@ -25,11 +25,16 @@ for node in ast.walk(ast.parse(current_file_str)):
             importlib.import_module(p)
         except ImportError:
             pip.main(["install", p])
+
+import json
+import jsons
+
+def debug_obj(obj):
+    j = jsons.dump(obj, indent=4)
+    print(j)
 #endregion
 
 #region Config
-import json
-
 root_path = os.path.dirname(os.path.abspath(__file__))
 buildin_adb_path = f"{root_path}/platform-tools/adb.exe"
 
@@ -52,6 +57,7 @@ else:
 
 print(f"Using environment: {config['name']}")
 adb_path = config["adb_path"] if "adb_path" in config else buildin_adb_path
+player = config["player"] if "player" in config else {}
 keys = config["keys"] if "keys" in config else {}
 #endregion
 
@@ -73,11 +79,10 @@ class Mapper:
 
 #region ADB
 class Adb:
-    def __init__(self, mapper):
+    def __init__(self):
         self.adb_prefix = f"\"{adb_path}\""
         self.screen_width = 0
         self.screen_height = 0
-        self.mapper = mapper
         self.__adb_setup()
 
     def __adb(self, cmd):
@@ -116,42 +121,22 @@ class Adb:
         self.adb_prefix = f"{self.adb_prefix} -s {target_device}"
         self.__adb_screen_size()
 
-    def adb_action(self, key):
-        release = False
-        if key[0] == "!":
-            release = True
-            key = key[1:]
-        act = self.mapper.map(key)
-        if act is None:
-            return
-        act = act.split()
-        act_type = act[0]
-        if act_type == "click":
-            self.adb_click(release, act[1], act[2])
-        elif act_type == "swipe":
-            self.adb_swipe(release, act[1], act[2], act[3], act[4])
-        elif act_type == "swipe_direction":
-            self.adb_swipe_direction(release, act[1], act[2])
-        elif act_type == "swipe_area":
-            self.adb_swipe_area(release, act[1], act[2])
-        else:
-            print(f"Unknown action: {act_type}")
-
-    def adb_click(self, release, x, y):
-        if release:
-            return
+    def click(self, release, x, y):
         x = float(x) * self.screen_width
         y = float(y) * self.screen_height
         self.__adb(f"shell input tap {x} {y}")
 
-    def adb_swipe(self, release, x, y, x_d, y_d):
-        if release:
-            return
+    def swipe(self, x, y, x_dst, y_dst):
         x = float(x) * self.screen_width
         y = float(y) * self.screen_height
-        x_d = float(x_d) * self.screen_width
-        y_d = float(y_d) * self.screen_height
-        self.__adb(f"shell input swipe {x} {y} {x_d} {y_d} 20")
+        x_dst = float(x_dst) * self.screen_width
+        y_dst = float(y_dst) * self.screen_height
+        self.__adb(f"shell input swipe {x} {y} {x_dst} {y_dst} 20")
+
+    def swipe_diff(self, x, y, x_diff, y_diff):
+        x = float(x) * self.screen_width
+        y = float(y) * self.screen_height
+        self.__adb(f"shell input swipe {x} {y} {x + x_diff} {y + y_diff} 20")
 #endregion
 
 #region Windows
@@ -159,34 +144,20 @@ import threading
 from pynput import keyboard as Keyboard, mouse as Mouse
 
 class Desktop:
-    def __init__(self, adb):
-        self.window_left = 0
-        self.window_top = 0
-        self.window_right = 0
-        self.window_bottom = 0
-        self.window_width = 0
-        self.window_height = 0
-
-        self.key_letters = [ False for _ in range(26) ]
-        self.key_digits = [ False for _ in range(10) ]
-        self.key_fns = [ False for _ in range(13) ]
+    def __init__(self):
         self.key_ctrl = False
         self.key_shift = False
         self.key_alt = False
-        self.key_space = False
+        self.key_records = set()
         self.key_worker = threading.Thread(target=self.key_work, args=(self,))
-        self.key_worker.start()
-
-        self.mouse_x = 0
-        self.mouse_y = 0
-        self.mouse_x_pct = 0.0
-        self.mouse_y_pct = 0.0
-        self.mouse_left = False
-        self.mouse_right = False
+        self.mouse_x = -1
+        self.mouse_y = -1
         self.mouse_worker = threading.Thread(target=self.mouse_work, args=(self,))
-        self.mouse_worker.start()
 
-        self.adb = adb
+    def start(self, conn):
+        self.conn = conn
+        self.key_worker.start()
+        self.mouse_worker.start()
 
     def key_work(arg, _):
         key_listener = Keyboard.Listener(
@@ -203,121 +174,81 @@ class Desktop:
         mouse_listener.start()
         mouse_listener.join()
 
-    def __calc_window_size(self):
-        self.window_width = self.window_right - self.window_left
-        self.window_height = self.window_bottom - self.window_top
-
-    def format_key(key):
-        if Desktop.is_ctrl(key):
-            return "ctrl"
-        elif Desktop.is_shift(key):
-            return "shift"
-        elif Desktop.is_alt(key):
-            return "alt"
-        elif Desktop.is_digit(key) >= 0:
-            return str(Desktop.is_digit(key))
-        elif Desktop.is_fn(key) >= 0:
-            return "f" + str(Desktop.is_fn(key))
+    def __format_key(self, vk, is_pressed):
+        if vk in self.key_records and is_pressed:
+            return None
+        if vk in self.key_records:
+            self.key_records.remove(vk)
         else:
-            a = Desktop.is_alpha(key)
-            if a >= 0:
-                return chr(a + ord('a'))
-        return ""
-
-    def __record_key(self, key, is_pressed):
-        fk = Desktop.format_key(key)
-        if fk == "":
-            return ""
-        elif fk == "ctrl":
+            self.key_records.add(vk)
+        if vk == 0xA2 or vk == 0xA3:
             self.key_ctrl = is_pressed
-        elif fk == "shift":
+            return None
+        if vk == 0xA0 or vk == 0xA1:
             self.key_shift = is_pressed
-        elif fk == "alt":
+            return None
+        if vk == 0xA4 or vk == 0xA5:
             self.key_alt = is_pressed
-        elif Desktop.is_digit(key) >= 0:
-            self.key_digits[Desktop.is_digit(key)] = is_pressed
-        elif Desktop.is_fn(key) >= 0:
-            self.key_fns[Desktop.is_fn(key)] = is_pressed
+            return None
+        if vk >= 0x70 and vk <= 0x7B:
+            # F1 ~ F12
+            return "F" + str(vk - 0x6F)
+        fk = ""
+        if is_pressed:
+            if self.key_ctrl:
+                fk += "C"
+            if self.key_shift:
+                fk += "S"
+            if self.key_alt:
+                fk += "A"
+        if vk >= 0x30 and vk <= 0x39:
+            # 0 ~ 9
+            fk += str(vk - 0x30)
+        elif vk >= 0x41 and vk <= 0x5A:
+            # a ~ z
+            fk += chr(vk + 0x20)
         else:
-            a = Desktop.is_alpha(key)
-            if a >= 0:
-                if a == ord("i") - ord("a") and self.is_ctrl and self.is_shift and self.is_alt:
-                    # record left-top corner
-                    self.window_left = self.mouse_x
-                    self.window_top = self.mouse_y
-                    self.__calc_window_size()
-                elif a == ord("o") - ord("a") and self.is_ctrl and self.is_shift and self.is_alt:
-                    # record right-bottom corner
-                    self.window_right = self.mouse_x
-                    self.window_bottom = self.mouse_y
-                    self.__calc_window_size()
-                self.key_letters[a] = is_pressed
-        return "!" + fk if not is_pressed else fk
+            return None
+        return fk if is_pressed else "!" + fk
+
+    def __to_vk(key):
+        if hasattr(key, "value"):
+            return key.value.vk
+        else:
+            return key.vk
 
     def on_press(self, key):
-        fk = self.__record_key(key, True)
-        self.adb.adb_action(fk)
-        # print(f"Key press is: {key}")
+        vk = Desktop.__to_vk(key)
+        fk = self.__format_key(vk, True)
+        if fk is not None:
+            self.conn.send(fk)
+            # print(f"Format Key: {fk}")
         return True
 
     def on_release(self, key):
-        fk = self.__record_key(key, False)
-        self.adb.adb_action(fk)
-        # print(f"Key release is: {key}")
+        vk = Desktop.__to_vk(key)
+        fk = self.__format_key(vk, False)
+        if fk is not None:
+            self.conn.send(fk)
+            # print(f"Format Key: {fk}")
         return True
 
     def on_move(self, x, y):
-        self.mouse_x = x
-        self.mouse_y = y
-
-        if self.window_width > 0:
-            self.mouse_x_pct = (x - self.window_left) / self.window_width
-        else:
-            self.mouse_x_pct = 0.0
-
-        if self.window_height > 0:
-            self.mouse_y_pct = (y - self.window_top) / self.window_height
-        else:
-            self.mouse_y_pct = 0.0
-        # print(f"Mouse position is: {x}, {y}")
-        return True
+        if x != self.mouse_x or y != self.mouse_y:
+            self.mouse_x = x
+            self.mouse_y = y
+            self.conn.send(f"M {x} {y}")
 
     def on_click(self, x, y, button, pressed):
-        if button == Mouse.Button.left:
-            self.mouse_left = pressed
-        elif button == Mouse.Button.right:
-            self.mouse_right = pressed
-        # print(f"Mouse click is: {x}, {y}, {button}, {pressed}")
+        if button == Mouse.Button.left and pressed:
+            self.conn.send(f"L {x} {y}")
+        elif button == Mouse.Button.right and pressed:
+            self.conn.send(f"R {x} {y}")
         return True
 
     def on_scroll(self, x, y, dx, dy):
         # print(f"Mouse scroll is: {x}, {y}, {dx}, {dy}")
         return True
-
-    def is_ctrl(key):
-        return key == Keyboard.Key.ctrl_l or key == Keyboard.Key.ctrl_r or key == Keyboard.Key.ctrl
-
-    def is_shift(key):
-        return key == Keyboard.Key.shift_l or key == Keyboard.Key.shift_r or key == Keyboard.Key.shift
-
-    def is_alt(key):
-        return key == Keyboard.Key.alt_l or key == Keyboard.Key.alt_r or key == Keyboard.Key.alt
-
-    def is_alpha(key):
-        s = str(key)
-        if len(s) == 3 and s[0] == "'" and s[2] == "'" and s[1].isalpha():
-            return ord(s[1]) - ord('a')
-        else:
-            return -1
-
-    def is_digit(key):
-        s = str(key)
-        if len(s) == 3 and s[0] == "'" and s[2] == "'" and s[1].isdigit():
-            return ord(s[1]) - ord('0')
-        else:
-            return -1
-
-    def is_fn(key):
         if key == Keyboard.Key.f1:
             return 1
         elif key == Keyboard.Key.f2:
@@ -346,11 +277,163 @@ class Desktop:
             return -1
 #endregion
 
-print("Start Running...")
+#region Reactor
+import multiprocessing as mp
+
+class Reactor:
+    def __init__(self, adb, mapper):
+        self.adb = adb
+        self.mapper = mapper
+
+        self.window_left = 0
+        self.window_top = 0
+        self.window_right = 0
+        self.window_bottom = 0
+        self.window_width = 0
+        self.window_height = 0
+
+        self.mouse_x = 0
+        self.mouse_y = 0
+        self.mouse_x_in = 0
+        self.mouse_y_in = 0
+        self.player_x_pct = player["x"] if "x" in player else 0.5
+        self.player_y_pct = player["y"] if "y" in player else 0.5
+        self.player_x = 0
+        self.player_y = 0
+        self.vector_x = 0
+        self.vector_y = 0
+
+    def desktop_start_entry(conn):
+        Desktop().start(conn)
+
+    def __setup_desktop(self):
+        mp.freeze_support()
+        parent_conn, child_conn = mp.Pipe()
+        self.desktop_process = mp.Process(
+            target=Reactor.desktop_start_entry, args=(child_conn,))
+        self.conn = parent_conn
+        self.child_conn = child_conn
+        self.desktop_process.start()
+
+    def loop(self):
+        self.__setup_desktop()
+        while True:
+            msg = self.conn.recv()
+            if msg == "exit":
+                self.desktop_process.terminate()
+                break
+            try:
+                self.__act(msg)
+            except Exception as e:
+                print(f"Exception: {e}")
+
+    def __act(self, fk):
+        release = False
+        if fk[0] == "!":
+            release = True
+            fk = fk[1:]
+        if self.__act_util(fk, release):
+            return
+        if self.__act_mouse(fk, release):
+            return
+        if self.__act_key(fk, release):
+            return
+
+    def __calc_window_size(self):
+        self.window_width = self.window_right - self.window_left
+        self.window_height = self.window_bottom - self.window_top
+        self.player_x = int(self.window_width * self.player_x_pct)
+        self.player_y = int(self.window_height * self.player_y_pct)
+
+    def __act_util(self, fk, release):
+        if fk == "CSAi":
+            # control + shift + alt + i
+            self.window_left = self.mouse_x
+            self.window_top = self.mouse_y
+            self.__calc_window_size()
+            print(f"Window Left-Top is: {self.mouse_x}, {self.mouse_y}")
+            return True
+        elif fk == "CSAo":
+            # control + shift + alt + o
+            self.window_right = self.mouse_x
+            self.window_bottom = self.mouse_y
+            self.__calc_window_size()
+            print(f"Window Right-Bottom is: {self.mouse_x}, {self.mouse_y}")
+            return True
+        else:
+            return False
+
+    def __unformat_key(self, fk):
+        is_ctrl = False
+        is_shift = False
+        is_alt = False
+        is_fn = False
+        if fk.startswith("C"):
+            fk = fk[1:]
+            is_ctrl = True
+        if fk.startswith("S"):
+            fk = fk[1:]
+            is_shift = True
+        if fk.startswith("A"):
+            fk = fk[1:]
+            is_alt = True
+        if fk.startswith("F"):
+            fk = fk[1:]
+            is_fn = True
+        return fk, is_ctrl, is_shift, is_alt, is_fn
+
+    def __act_key(self, fk, release):
+        if release:
+            return
+        k, is_ctrl, is_shift, is_alt, is_fn = self.__unformat_key(fk)
+        act = self.mapper.map(k)
+        if act is None:
+            return
+        act = act.split()
+        act_type = act[0]
+        if act_type == "click":
+            self.adb.click(act[1], act[2])
+        elif act_type == "swipe":
+            self.adb.swipe(act[1], act[2], act[3], act[4])
+        elif act_type == "swipe_direction":
+            self.adb.swipe_diff(act[1], act[2], self.vector_x, self.vector_y)
+        elif act_type == "swipe_area":
+            self.adb.swipe_area(release, act[1], act[2])
+        else:
+            print(f"Unknown action: {act_type}")
+
+    def __act_mouse(self, fk, release):
+        is_move = fk.startswith("M")
+        is_left = fk.startswith("L")
+        is_right = fk.startswith("R")
+        if is_move or is_left or is_right:
+            fk = fk.split()
+            x = int(fk[1])
+            y = int(fk[2])
+        else:
+            return False
+        self.mouse_x = x
+        self.mouse_y = y
+        if x <= self.window_left or x >= self.window_right or y <= self.window_top or y >= self.window_bottom:
+            return True
+        if is_left or is_right:
+            #todo
+            pass
+        self.mouse_x_in = x - self.window_left
+        self.mouse_y_in = y - self.window_top
+        self.vector_x = self.mouse_x_in - self.player_x
+        self.vector_y = self.mouse_y_in - self.player_y
+        # print(f"Vector is: {self.vector_x}, {self.vector_y}")
+        # print(f"Mouse position is: {self.mouse_x_in}, {self.mouse_y_in}")
+        return True
+#endregion
+
 mapper = Mapper(keys)
-adb = Adb(mapper)
-desk = Desktop(adb)
+adb = Adb()
+reactor = Reactor(adb, mapper)
 
-while True:
-    time.sleep(1)
-
+if __name__ == "__main__":
+    print("Start Running...")
+    reactor.loop()
+    while True:
+        time.sleep(1)
